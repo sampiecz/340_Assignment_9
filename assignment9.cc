893fb5d57188b962040f70b084b038b0282057c0
sampiecz <samuel.piecz@gmail.com>
2018-05-01 22:25:38 -0500

    Started on constructor. Did get_size, then loaded other functions with couts to test constructor first.

--- assignment9.cc
+++ assignment9.cc
@@ -15,6 +15,12 @@
 #include <list>
 #include "assignment9.h"
 
+using std::ifstream;
+using std::vector;
+using std::cout;
+using std::cerr;
+using std::endl;
+
 /***************************************************************
   
  Name: depth_first
@@ -28,6 +34,7 @@
  ***************************************************************/
 void graph::depth_first( int );
 {
+    cout << "\nDFS\n" << endl;
 }
 
 /***************************************************************
@@ -43,6 +50,46 @@ void graph::depth_first( int );
  ***************************************************************/
 graph::graph( const char* filename );
 {
+    // Get the file as an ifstream object called infile
+    ifstream infile(filename, ios::in);
+    // Initialize string to hold each line of file
+    string line;
+
+    // Initialize counter
+    int counter = 0;
+
+    // Loop through all lines while you can keep getting a line
+    while (getline(infile, line))
+    {
+        // if first iteration set size
+        if (counter == 0)
+        {
+            size = int(line);
+        }
+        // otherwise do something with labels
+        else if (counter == 1)
+        {
+            for (int index = 0; index < sizeof(line); index++)
+            {
+                labels.push_back(line[index]);
+            }
+        }
+        // if none of these things need to set index values to position
+        // inside main vector, sub the specific vertex
+        else
+        {
+            for (int i = 0; i < sizeof(line); i++)
+            {
+                cout << line[i] << endl;
+            }
+        }
+
+        // Increment counter 
+        counter++;
+    }
+
+    cout << size << endl;
+
 }
 
 /***************************************************************
@@ -73,6 +120,7 @@ graph::~graph();
  ***************************************************************/
 int graph::get_size() const;
 {
+    return size;
 }
 
 /***************************************************************
@@ -88,6 +136,7 @@ int graph::get_size() const;
  ***************************************************************/
 void graph::traverse( ) ;
 {
+    cout << "\nTraverse method\n" << endl;
 }
 
 /***************************************************************
@@ -103,7 +152,10 @@ void graph::traverse( ) ;
  ***************************************************************/
 void graph::print ( ) const;
 {
+    cout << "\nPrint Method\n" << endl;
 }
+
+
 
 #define ASSIGNMENT9_TEST
 #ifdef 	ASSIGNMENT9_TEST
